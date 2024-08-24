c1c82456ba3482cf6f10c8acb347eb2b81a2a284
osaro123 <osarolawani983@gmail.com>
2024-08-24 14:16:45 +0100

    initial commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='absolute bottom-0 left-0 px-4'>
+    <div className='absolute bottom-0 left-0 px-4 flex justify-between'>
         <h1 className='text-9xl'>Frontend <br /> Engineer</h1>
     </div>
   )
